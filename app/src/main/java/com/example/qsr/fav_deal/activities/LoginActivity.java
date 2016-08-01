40f39658bb44b9c35eda4a1517bd18fd90ba87dc
爱死了李清照 <249046607@qq.com>
2016-08-02 01:38:13 +0800

    个人中心界面的改动

---
+++ app/src/main/java/com/example/qsr/fav_deal/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.qsr.fav_deal.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.qsr.fav_deal.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+    }
+}
